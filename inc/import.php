0ad1412c2a64952c0a50466dd477dee9b4d18ca1
Qrac <qrac.jp@gmail.com>
2019-06-14 10:07:45 +0900

    Fix import style

--- inc/import.php
+++ inc/import.php
@@ -6,7 +6,7 @@
 // Add files
 function add_files() {
   if ( !is_admin() ) {
-    wp_css('style', '/style.css');
+    wp_enqueue_style( 'style', get_template_directory_uri() . '/style.css' );
   }
 }
 add_action('wp_enqueue_scripts', 'add_files', 1);
