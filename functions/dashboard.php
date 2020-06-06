6d590685b457bde4d8117e03e3260edd82bc9901
Qrac <qrac.jp@gmail.com>
2020-06-06 12:33:14 +0900

    New design

--- functions/dashboard.php
+++ functions/dashboard.php
@@ -5,7 +5,7 @@
 
 // Add favicon
 function admin_favicon() {
-  echo '<link rel="icon" href="'.get_template_directory_uri().'/assets/images/apple-touch-icon.png" />';
-  echo '<link rel="icon" href="'.get_template_directory_uri().'/assets/images/favicon.png" />';
+  echo '<link rel="icon" href="'.get_template_directory_uri().'/apple-touch-icon.png" />';
+  echo '<link rel="icon" href="'.get_template_directory_uri().'/favicon.png" />';
 }
 add_action( 'admin_head', 'admin_favicon' );
