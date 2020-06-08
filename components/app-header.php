f1775e9ffaaa0455d37b75a702204325f78c9066
Qrac <qrac.jp@gmail.com>
2020-06-08 19:30:58 +0900

    Add menu #11

--- components/app-header.php
+++ components/app-header.php
@@ -5,5 +5,14 @@
 ?>
 <header>
   <h1><?php bloginfo('name'); ?></h1>
-  <nav><a href="<?php echo esc_url(home_url('/')); ?>">Home</a></nav>
+  <nav>
+    <a href="<?php echo esc_url(home_url('/')); ?>">Home</a>
+    <?php wp_nav_menu(array(
+      'container' => false,
+      'theme_location' => 'menu_header_main',
+      'items_wrap' => '%3$s',
+      'depth' => 1,
+      'walker' => new custom_walker_nav_menu
+    )); ?>
+  </nav>
 </header>
