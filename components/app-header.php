2077b03a41e233eb05043ee7af5acf90ff14a811
Qrac <qrac.jp@gmail.com>
2020-06-08 22:14:09 +0900

    Fix menu

--- components/app-header.php
+++ components/app-header.php
@@ -7,12 +7,14 @@
   <h1><?php bloginfo('name'); ?></h1>
   <nav>
     <a href="<?php echo esc_url(home_url('/')); ?>">Home</a>
-    <?php wp_nav_menu(array(
-      'container' => false,
-      'theme_location' => 'menu_header_main',
-      'items_wrap' => '%3$s',
-      'depth' => 1,
-      'walker' => new custom_walker_nav_menu
-    )); ?>
+    <?php if( has_nav_menu('menu_header_main') ) : ?>
+      <?php wp_nav_menu(array(
+        'container' => false,
+        'theme_location' => 'menu_header_main',
+        'items_wrap' => '%3$s',
+        'depth' => 1,
+        'walker' => new custom_walker_nav_menu
+      )); ?>
+    <?php endif; ?>
   </nav>
 </header>
