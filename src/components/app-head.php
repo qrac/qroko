1e4fb8c3d184a464d2e09cce2b46edc4a892fb5b
Qrac <qrac.jp@gmail.com>
2020-10-11 20:26:42 +0900

    Fix template

--- src/components/app-head.php
+++ src/components/app-head.php
@@ -5,7 +5,7 @@
 ?>
 <head>
   <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="format-detection" content="telephone=no, email=no, address=no">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <?php wp_head(); ?>
