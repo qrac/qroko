beadad82f39af15886d80293e7e9a2dc620a81c8
Qrac <qrac.jp@gmail.com>
2020-12-12 23:04:31 +0900

    Support align wide

--- functions/editors.php
+++ functions/editors.php
@@ -9,6 +9,9 @@ add_theme_support('wp-block-styles');
 // Support editor styles
 add_theme_support('editor-styles');
 
+// Support align wide
+add_theme_support('align-wide');
+
 // Import editor styles
 function qroko_import_editor_styles() {
   add_editor_style('assets/css/theme-variable.css');
