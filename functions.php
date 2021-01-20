7a46aec88b2fe08dd389ce4424e8efe4881e9e9e
Qrac <qrac.jp@gmail.com>
2021-01-20 20:36:28 +0900

    Aggregate add_theme_support into an action hook

--- functions.php
+++ functions.php
@@ -11,5 +11,4 @@ require_once('functions/menu.php');
 require_once('functions/pager.php');
 require_once('functions/pagination.php');
 require_once('functions/support.php');
-require_once('functions/thumbnail.php');
 require_once('functions/widget.php');
