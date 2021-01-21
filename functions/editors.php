e1d9014a2a5c44eed5aa9fd589b9878dc0ef48fb
Qrac <qrac.jp@gmail.com>
2021-01-21 11:07:28 +0900

    Change the way inline CSS is loaded

--- functions/editors.php
+++ functions/editors.php
@@ -3,23 +3,41 @@
 // Editors
 //----------------------------------------------------
 
+// Enqueue editor css
+function qroko_enqueue_editor_css($css_name, $file_path) {
+  wp_enqueue_style(
+    $css_name, get_template_directory_uri() . $file_path, array(),
+    date('YmdGis', filemtime(get_template_directory() . $file_path))
+  );
+}
+
+// Create editor inline css
+function qroko_create_editor_inline_css() {
+  $output = '';
+
+  $max_site_width = qroko_theme_mod_max_site_width();
+  if ($max_site_width != null && $max_site_width != 620 && $max_site_width >= 300) {
+    $output .= ":root { --theme-site-width: {$max_site_width}px; }";
+  }
+
+  if($output == '') return false;
+  $str = array("\t", "\r\n", "\r", "\n");
+  $output = str_replace($str, '', $output);
+
+  return $output;
+}
+
 // Import editor styles
 function qroko_import_editor_styles() {
-  add_editor_style('assets/css/theme-variable.css');
-  add_editor_style('assets/css/theme-light.css');
-  add_editor_style('assets/css/theme-dark.css');
+  qroko_enqueue_editor_css('qroko-editor-style-theme-light', '/assets/css/theme-light.css');
+  qroko_enqueue_editor_css('qroko-editor-style-theme-dark', '/assets/css/theme-dark.css');
+  qroko_enqueue_editor_css('qroko-editor-style-theme-variable', '/assets/css/theme-variable.css');
+  wp_add_inline_style('qroko-editor-style-theme-variable', qroko_create_front_inline_css());
+}
+add_action('enqueue_block_editor_assets', 'qroko_import_editor_styles');
+
+// Conversion import editor styles
+function qroko_conversion_import_editor_styles() {
   add_editor_style('assets/css/editor.css');
 }
-add_action('after_setup_theme', 'qroko_import_editor_styles');
-
-// Custom editor max site width
-function qroko_custom_editor_max_site_width() {
-  $max_site_width = get_option('qroko_max_site_width');
-
-  if ($max_site_width != null && $max_site_width > 0) {
-    echo '<style>' .
-      ':root .editor-styles-wrapper { --theme-site-width: ' . $max_site_width . 'px; }' .
-      '</style>';
-  }
-}
-add_action('admin_head', 'qroko_custom_editor_max_site_width');
+add_action('after_setup_theme', 'qroko_conversion_import_editor_styles');
--- functions/fronts.php
+++ functions/fronts.php
@@ -11,30 +11,35 @@ function qroko_enqueue_front_css($css_name, $file_path) {
   );
 }
 
+// Create front inline css
+function qroko_create_front_inline_css() {
+  $output = '';
+
+  $max_site_width = qroko_theme_mod_max_site_width();
+  if ($max_site_width != null && $max_site_width != 620 && $max_site_width >= 300) {
+    $output .= ":root { --theme-site-width: {$max_site_width}px; }";
+  }
+
+  if($output == '') return false;
+  $str = array("\t", "\r\n", "\r", "\n");
+  $output = str_replace($str, '', $output);
+
+  return $output;
+}
+
 // Import front styles
 function qroko_import_front_styles() {
   if (!is_admin()) {
-    qroko_enqueue_front_css('qroko-front-style-theme-variable', '/assets/css/theme-variable.css');
     qroko_enqueue_front_css('qroko-front-style-theme-light', '/assets/css/theme-light.css');
     qroko_enqueue_front_css('qroko-front-style-theme-dark', '/assets/css/theme-dark.css');
+    qroko_enqueue_front_css('qroko-front-style-theme-variable', '/assets/css/theme-variable.css');
     qroko_enqueue_front_css('qroko-front-style-front', '/assets/css/front.css');
     $hide_post_comment = qroko_theme_mod_hide_post_comment();
     if (!$hide_post_comment) {
       qroko_enqueue_front_css('qroko-front-style-comment', '/assets/css/comment.css');
     }
     qroko_enqueue_front_css('qroko-front-style', '/style.css');
+    wp_add_inline_style('qroko-front-style', qroko_create_front_inline_css());
   }
 }
 add_action('wp_enqueue_scripts', 'qroko_import_front_styles');
-
-// Custom front max site width
-function qroko_custom_front_max_site_width() {
-  $max_site_width = get_option('qroko_max_site_width');
-
-  if ($max_site_width != null && $max_site_width > 0) {
-    echo '<style>' .
-      ':root body { --theme-site-width: ' . $max_site_width . 'px; }' .
-      '</style>';
-  }
-}
-add_action('wp_head', 'qroko_custom_front_max_site_width');
